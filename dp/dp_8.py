c114befe31a25e2c71bdfef9e3ca34b2962c493d
mgKang3646 <lordofkangs@naver.com>
2023-06-09 02:11:59 +0000

    "BOJ 문제풀이 (DP) "

---
+++ dp/dp_8.py
@@ -0,0 +1,16 @@
+
+import sys
+input = sys.stdin.readline
+
+n = int(input())
+
+d = [0]*1001
+
+d[1] = 1
+d[2] = 2 #경계값 주의
+
+for i in range(3,n+1) :
+  d[i] = d[i-2] + d[i-1]
+
+print(d[n]%10007)
+
