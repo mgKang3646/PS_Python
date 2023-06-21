5192738378b054c625c7ffd3911f4f31608d9010
mgKang3646 <lordofkangs@naver.com>
2023-06-21 02:22:48 +0000

    세그먼트 트리 구현

---
+++ prefixsum/segment_tree.py
@@ -0,0 +1,23 @@
+
+
+arr = list(map(int,input().split())) #수열
+tree = [0]*(len(arr)*2) #세그먼트트리
+
+#세그먼트트리 구현
+def init_tree(start,end,node) :
+  if start == end : #리프노드인 경우
+    tree[node] = arr[start]
+    return tree[node]
+  #리프노드가 아닌경우
+  mid = (start+end)//2 #이진탐색
+  tree[node] = init_tree(start,mid,2*node) + init_tree(mid+1,end,2*node+1) #재귀호출
+  return tree[node]
+
+init_tree(0,len(arr)-1,1)
+print(tree)
+  
+
+
+
+
+           
