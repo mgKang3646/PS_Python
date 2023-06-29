cac7d6a42fa3af6179f95631b0cca2e06c16f7da
mgKang3646 <lordofkangs@naver.com>
2023-06-29 00:14:05 +0000

    BOJ1080 행렬 ( greedy )

--- greedy/greedy_7.py
+++ greedy/greedy_7.py
@@ -2,43 +2,42 @@
 #BOJ1080 행렬 ( greedy )
 n,m = map(int,input().split())
 
-mx1 = [[0] for _ in range(n)]
-mx2 = [[0] for _ in range(n)]
+mx1 = [[0] for _ in range(n)] #행렬1
+mx2 = [[0] for _ in range(n)] #행렬2
 
 for i in range(n) :
   mx1[i] = list(map(int,input().rstrip("\n")))
 for i in range(n) :
   mx2[i] = list(map(int,input().rstrip("\n")))
 
-dx = [0,1,2,0,1,2,0,1,2]
+#3X3 좌표이동 리스트
+dx = [0,1,2,0,1,2,0,1,2] 
 dy = [0,0,0,1,1,1,2,2,2]
 
-def isvalidate(x,y) :
-  if x >=0 and y>=0 and x<m and y<n : return True
-  else : return False
-
-#일치여부 확인
-def issame() :
-  for i in range(n) :
-    for j in range(m) :
-      if mx1[i][j] != mx2[i][j] : return False
-  return True
-
-#뒤집기
+# 행렬 뒤집는 함수
 def reverse_mx(x,y) :
   for i in range(9) :
     nx = x+dx[i]
     ny = y+dy[i] 
     mx1[ny][nx] = 1 - mx1[ny][nx] 
 
-count = 0
-for i in range(n-2) :
+#두 행렬 일치여부 확인 함수
+def issame() :
+  for i in range(n) :
+    for j in range(m) :
+      if mx1[i][j] != mx2[i][j] : return False
+  return True
+  
+#풀이
+ans = 0
+for i in range(n-2) : 
   for j in range(m-2) :
     if mx1[i][j] != mx2[i][j] :
       reverse_mx(j,i)
-      count += 1
+      ans += 1
 
-if issame() : print(count)
+#출력
+if issame() : print(ans)
 else : print(-1)
   
   
